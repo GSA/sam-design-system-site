c1121762f9756e5fd6f2d95bf29d66d032750bc6
Josh Bruce <josh@8foldproductivity.com>
2016-07-31 16:00:21 -0400

    Gulp images migrated and confirmed

--- config/gulp/images.js
+++ config/gulp/images.js
@@ -1,18 +1,16 @@
 var gulp = require('gulp');
-var del = require('del');
 var dutil = require('./doc-util');
 var task = /([\w\d-_]+)\.js$/.exec(__filename)[ 1 ];
-
-gulp.task('clean-assets-img', function() {
-  return del(['assets/img/**/*', '!assets/img/home/**']);
-});
 
 gulp.task(task, function (done) {
 
   dutil.logMessage(task, 'Copying Images');
 
-  var stream = gulp.src(['node_modules/uswds/src/img/**/*', 'src/img/**/*', '_styleguide_assets/**/*'])
-    .pipe(gulp.dest('assets/img'));
+  var stream = gulp.src([
+      'node_modules/uswds/src/img/**/*', 
+      'src/img/**/*', 
+    ])
+    .pipe(gulp.dest('dist/img'));
 
   return stream;
 

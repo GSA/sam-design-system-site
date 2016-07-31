fb780d5f1fa187ef2767abe29678913d3b5aa2ce
Josh Bruce <josh@8foldproductivity.com>
2016-07-31 15:53:41 -0400

    Gulp doc-util verified

--- config/gulp/build.js
+++ config/gulp/build.js
@@ -44,20 +44,3 @@ gulp.task('build', function (done) {
   );
 
 });
-
-// var gulp = require('gulp');
-// var dutil = require('./doc-util');
-// var clean = require('gulp-clean');
-// var runSequence = require( 'run-sequence' );
-
-// gulp.task('build', function (done) {
-//   runSequence(
-//     [
-//       'sass',
-//       'javascript',
-//       'images',
-//       'fonts',
-//     ],
-//     done
-//   );
-// });

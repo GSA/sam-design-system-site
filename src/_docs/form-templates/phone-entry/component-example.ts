78099e35efe74891335c5261ed467c41e447f6ee
Diego Ruiz <diego.ruiz@reisystems.com>
2017-01-20 16:07:26 -0500

    phone-entry updates

--- src/_docs/form-templates/phone-entry/component-example.ts
+++ src/_docs/form-templates/phone-entry/component-example.ts
@@ -9,17 +9,23 @@ import { BaseExampleComponent } from '../../baseexample.component';
 	selector: 'doc-phone-entry',
   template: `
 <doc-template [markdown]="markdown" [example]="example" [typedoc]="typedoc_content">
-<samPhoneEntry [(model)]="phoneModel" (emitter)="phoneModelChange($event)"></samPhoneEntry>
+<samPhoneEntry [phoneNumberTemplate]="'___-___-____'" [(model)]="phoneModel" (emitter)="phoneModelChange($event)"></samPhoneEntry>
+<samPhoneEntry [(model)]="phoneModel2" (emitter)="phoneModel2Change($event)"></samPhoneEntry>
 </doc-template>
 `
 })
 export class PhoneEntryExampleComponent extends BaseExampleComponent implements OnInit {
-	phoneModel = "1+(123)456-3366";
+	phoneModel = "123-456-3366";
+  phoneModel2 = "1+(123)456-3366";
   typedoc_target = "/phone-entry.component";
   typedoc_content = "";
   markdown = require("html-loader!markdown-loader!./documentation.md");
-	example = `<samPhoneEntry [(model)]="phoneModel" (emitter)="phoneModelChange($event)"></samPhoneEntry>`;
+	example = `<samPhoneEntry [phoneNumberTemplate]="'___-___-____'" [(model)]="phoneModel" (emitter)="phoneModelChange($event)"></samPhoneEntry>
+<samPhoneEntry [(model)]="phoneModel2" (emitter)="phoneModel2Change($event)"></samPhoneEntry>`;
 	phoneModelChange(phoneNum){
     this.phoneModel = phoneNum;
+  }
+  phoneModel2Change(phoneNum){
+    this.phoneModel2 = phoneNum;
   }
 }

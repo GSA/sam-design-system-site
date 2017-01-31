bb608387155ef4dbc79967e609f06ebf18ef5589
Colin Alford <colinalford@gmaill.com>
2017-01-31 10:51:14 -0500

    Removed unneeded changes from ui-kit

--- sam-ui-elements/src/ui-kit/types.ts
+++ sam-ui-elements/src/ui-kit/types.ts
@@ -4,17 +4,3 @@ export type OptionsType = Array<{
   name: string, // the machine readable description of the input
   disabled?: boolean // if true, the option is greyed out and not clickable
 }>;
-
-/**
- * Interface to be used with SamSidenav. 
- */
-export interface MenuItem {
-  label: string;
-  route?: string;
-  children?: MenuItem[];
-}
-
-export interface Test {
-  requiredVar: string;
-  notRequiredVar?: string;
-}

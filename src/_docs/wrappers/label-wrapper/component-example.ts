b7490b6116cc94aabaaa2e4b13f5d0a4ae130810
diegoaruiz <diego@reisystems.com>
2017-08-23 16:13:01 -0400

    adding some documentation for wrappers

--- src/_docs/wrappers/label-wrapper/component-example.ts
+++ src/_docs/wrappers/label-wrapper/component-example.ts
@@ -13,11 +13,14 @@ import {
 import { BaseExampleComponent } from '../../baseexample.component';
 
 //tabs/spacing matters for code example block
-var code_example = 'TODO';
+var code_example = `<sam-label-wrapper label="Fieldset Label Example" hint="Hint text goes here">
+  <sam-text label="Field #1" name="field1"></sam-text>
+</sam-label-wrapper>`;
+
 
 @Component({
   selector: 'doc-sam-label-wrapper',
-  template: '<doc-template [markdown]="markdown" [example]="example" [typedoc]="typedoc_content"> + code_example + </doc-template>'
+  template: '<doc-template [markdown]="markdown" [example]="example" [typedoc]="typedoc_content">' + code_example + '</doc-template>'
 })
 export class LabelWrapperExampleComponent extends BaseExampleComponent implements OnInit {
   typedoc_target = "LabelWrapper";

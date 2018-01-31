993c6a8c706374e5d0b4c324782f64ad94682147
Carlos Valle <carlos.m.valle@gmail.com>
2018-01-31 15:27:15 -0500

    Improving sam-list and sam-icon

--- src/patterns/pages/pages.service.ts
+++ src/patterns/pages/pages.service.ts
@@ -22,21 +22,34 @@ export class PagesService {
 
     contentSecondaryBoxList: [
       {
-        "icon": "video",
+        "icon": "none",
         "link": "#",
         "text": "Orci varius natoque"
       },
       {
-        "icon": "video",
+        "icon": "none",
         "link": "#",
         "text": "Vivamus ac est eget ipsum"
       }
     ],
 
-    contentBelowBoxes: 'Sed vestibulum ex rhoncus, feugiat risus laoreet, imperdiet quam. Aliquam erat volutpat.',
+    contentBelowBoxes: 'Sed vestibulum ex rhoncus, feugiat risus laoreet, \
+    imperdiet quam. Aliquam erat volutpat.',
 
     contentFirstParagraphTitle: 'Pharetra Vel Lacus Vitae',
-    contentFirstParagraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis commodo bibendum.',
+    contentFirstParagraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing \
+    elit. Donec lobortis commodo bibendum.',
+    contentFirstParagraphList: [
+      {"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit"},
+      {"text": "Etiam dignissim enim eget suscipit auctor"},
+      {"text": "Donec eget elit ullamcorper est malesuada"},
+      {"text": "Duis sit amet felis porta, dapibus lectus"}
+    ],
+    contentFirstParagraphList2: [
+      {"text": "Mauris luctus urna at semper fermentum"},
+      {"text": "Ut suscipit nisl sit amet tempus bibendum"},
+      {"text": "Nam eget risus ornare, varius enim nec"}
+    ],
 
     contentSecondParagraphTitle: 'Pharetra Vel Lacus Vitae',
     contentSecondParagraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing \
@@ -50,22 +63,22 @@ export class PagesService {
     contentHorizontalMenuTitle: 'Luctus Velit: Integer fermentum',
     contentHorizontalMenuItems: [
       {
-        "icon": "video",
+        "icon": "none",
         "link": "#",
         "text": "Lorem"
       },
       {
-        "icon": "video",
+        "icon": "none",
         "link": "#",
         "text": "Suspendisse"
       },
       {
-        "icon": "video",
+        "icon": "none",
         "link": "#",
         "text": "Maecenas"
       },
       {
-        "icon": "video",
+        "icon": "none",
         "link": "#",
         "text": "Vivamus"
       }
@@ -103,22 +116,33 @@ export class PagesService {
     ],
 
     contentBelowBoxes: 'SAM Users can generally be grouped in to three types: \
-    federal users, award recipients (entity users), and public users.',
+    federal users, award recipients <em>(entity users)</em>, and public users.',
 
     contentFirstParagraphTitle: 'What You Can Do Without An Account',
     contentFirstParagraph: 'You can search for the following public information \
     without a user account.',
+    contentFirstParagraphList: [
+      {"text": "Federal assistance listings"},
+      {"text": "Contract opportunities"},
+      {"text": "Contract awards"},
+      {"text": "Entity registrations"}
+    ],
+    contentFirstParagraphList2: [
+      {"text": "Entity exclusions"},
+      {"text": "Federal hierarchy"},
+      {"text": "Wage determinations"}
+    ],
 
     contentSecondParagraphTitle: 'Sign Up Now',
     contentSecondParagraph: "While you won't be able to access any additional \
     capabilities yet, you can prepare by creating your account now. When the \
     additional features become ready in the upcoming months, you'll be ready \
     to sign in.",
-    contentSecondParagraph2: "Note: Even if you have an existing account in one \
-    of the legacy systems (e.g., SAM.gov, FBO.gov, CFDA.gov) you'll need to \
-    create a new account for beta.SAM.gov. In the future, after all of the \
-    individual systems are retired, this account will be your only account for \
-    all of our features",
+    contentSecondParagraph2: "<strong>Note:</strong> Even if you have an existing \
+    account in one of the legacy systems <em>(e.g., SAM.gov, FBO.gov, CFDA.gov)</em>\
+    you'll need to create a new account for beta.SAM.gov. In the future, after \
+    all of the individual systems are retired, this account will be your only \
+    account for all of our features",
 
     contentHorizontalMenuTitle: 'Learning Center: User Accounts',
     contentHorizontalMenuItems: [
@@ -128,17 +152,17 @@ export class PagesService {
         "text": "Video"
       },
       {
-        "icon": "video",
+        "icon": "comment",
         "link": "#",
         "text": "FAQs"
       },
       {
-        "icon": "video",
+        "icon": "newspaper",
         "link": "#",
         "text": "Articles"
       },
       {
-        "icon": "video",
+        "icon": "book",
         "link": "#",
         "text": "Definitions"
       }
@@ -146,8 +170,8 @@ export class PagesService {
   }
 
   PageA = {
-    data: this.loremData,
-    value: 'lorem'
+    data: this.helpData,
+    value: 'help'
   }
 
   updatePageData(page, value){

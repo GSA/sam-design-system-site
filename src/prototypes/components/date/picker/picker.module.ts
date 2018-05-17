ac9198017fbf7b533fc6a3e2eb8c021ade3c0349
diegoaruiz <diego@reisystems.com>
2018-05-17 13:29:55 -0400

    adding 4th example with 'forked' 3rd party

---
+++ src/prototypes/components/date/picker/picker.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule }   from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { SamUIKitModule } from 'sam-ui-elements/src/ui-kit';
+import { DatepickerComponent } from './picker.component';
+
+export * from './picker.component';
+
+@NgModule({
+  declarations: [ DatepickerComponent ],
+  exports: [ DatepickerComponent ],
+  imports: [ SamUIKitModule, CommonModule, FormsModule, ReactiveFormsModule ]
+})
+export class DatepickerProtoModule { }

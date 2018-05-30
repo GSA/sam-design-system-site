81eb2cc18cc8f7f97e2956edcb8a1c66ba190f6c
Zeshan Ahmad <z.ahmad@ibm.com>
2018-05-30 17:40:47 -0400

    First Commit
    Added stylesheet and adjusted html code.

--- src/patterns/pages/layout/layout.component.ts
+++ src/patterns/pages/layout/layout.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sam-layout-demo-component',
-  templateUrl: './layout.template.html'
+  templateUrl: './layout.template.html',
+  styleUrls: ['./css/ztest.css']
 })
 export class SamLayoutDemoComponent {}

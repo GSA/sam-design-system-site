8743ad94b66dbf2aa72e710bc98e48da15a37a16
Colin Alford <colinalford@gmail.com>
2018-05-31 10:25:01 -0400

    Moved layout styles to styles folder

--- src/patterns/pages/layout/layout.component.ts
+++ src/patterns/pages/layout/layout.component.ts
@@ -1,8 +1,46 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  AfterContentInit
+} from '@angular/core';
+
+@Component({
+  selector: 'sam-action-bar',
+  template: '<ng-content></ng-content>'
+})
+export class SamActionBarComponent {}
+
+@Component({
+  selector: 'sam-aside',
+  template: '<ng-content></ng-content>'
+})
+export class SamAsideComponent {}
+
+@Component({
+  selector: 'sam-main',
+  template: '<ng-content></ng-content>'
+})
+export class SamMainComponent {}
+
+@Component({
+  selector: 'sam-layout',
+  template: '<ng-content></ng-content>'
+})
+export class SamLayoutComponent implements AfterContentInit {
+  @ContentChild(SamActionBarComponent)
+  public actions: SamActionBarComponent;
+
+  @ContentChild(SamAsideComponent)
+  public aside: SamAsideComponent;
+
+  @ContentChild(SamMainComponent)
+  public main: SamMainComponent;
+
+  public ngAfterContentInit () {}
+}
 
 @Component({
   selector: 'sam-layout-demo-component',
-  templateUrl: './layout.template.html',
-  styleUrls: ['./css/ztest.css']
+  templateUrl: './layout.template.html'
 })
 export class SamLayoutDemoComponent {}

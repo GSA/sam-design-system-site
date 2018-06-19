3f396a82e8f7f462a9982bf39044c3233543451c
diegoaruiz <diego@reisystems.com>
2018-06-19 11:25:25 -0400

    tweak to test way of programatically defining columns

--- src/patterns/pages/layout/layout.component.ts
+++ src/patterns/pages/layout/layout.component.ts
@@ -41,6 +41,43 @@ export class SamLayoutDemoComponent implements OnInit {
   public pagination: Observable<any>;
   public data: Observable<any>;
   public length: number;
+  public columnDefObs = Observable.of([{
+    'def': 'Agency',
+    'headerLabel': 'Agency',
+    'cellId': 'Agency'
+  },{
+    'def': 'CFDANumber',
+    'headerLabel': 'CFDA Number',
+    'cellId': 'CFDA Number'
+  },{
+    'def': 'Title',
+    'headerLabel': 'Title',
+    'cellId': 'Title'
+  },{
+    'def': 'CurrentStatus',
+    'headerLabel': 'Current Status',
+    'cellId': 'Current Status'
+  },{
+    'def': 'LastUpdatedDate',
+    'headerLabel': 'Last Updated Date',
+    'cellId': 'Last Updated Date'
+  },{
+    'def': 'ObligationsUpdated',
+    'headerLabel': 'Obligations Updated',
+    'cellId': 'Obligations Updated'
+  },{
+    'def': 'OMBReviewDate',
+    'headerLabel': 'OMB Review Date',
+    'cellId': 'OMB Review Date'
+  },{
+    'def': 'LastPublishedDate',
+    'headerLabel': 'Last Published Date',
+    'cellId': 'Last Published Date'
+  },{
+    'def': 'AutoPublished',
+    'headerLabel': 'Auto Published',
+    'cellId': 'Auto Published'
+  }]);
 
   @ViewChild(SamSortDirective) _sort: SamSortDirective;
   @ViewChild(SamModalComponent) fieldsEditor: SamModalComponent;

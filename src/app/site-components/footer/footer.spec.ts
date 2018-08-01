1762e039ec10d0d74b832d122777fa4ba35be0e4
diegoaruiz <diego@reisystems.com>
2018-08-01 17:14:23 -0400

    more tests

---
+++ src/app/site-components/footer/footer.spec.ts
@@ -0,0 +1,32 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import {
+  inject,
+  async,
+  TestBed,
+  ComponentFixture
+} from '@angular/core/testing';
+
+// Load the implementations that should be tested
+import { SiteFooterComponent } from './footer.component';
+
+describe(`SiteFooterComponent tests`, () => {
+  let comp: SiteFooterComponent;
+  let fixture: ComponentFixture<SiteFooterComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SiteFooterComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
+    });
+    fixture = TestBed.createComponent(SiteFooterComponent);
+    comp    = fixture.componentInstance;
+    fixture.detectChanges(); // trigger initial data binding
+  });
+
+  it(`should be initialized`, () => {
+    expect(fixture).toBeDefined();
+    expect(comp).toBeDefined();
+  });
+
+
+});

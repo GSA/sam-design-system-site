0edd20d0e7b0849998921e390a1049d3c20398c9
Colin Alford <colinalford@gmail.com>
2018-08-01 17:27:14 -0400

    Added spec for markdown service

--- src/app/services/markdown/markdown.service.spec.ts
+++ src/app/services/markdown/markdown.service.spec.ts
@@ -2,14 +2,14 @@
 import { MarkdownProcessor } from './markdown-processor';
 
 describe('Sam Design System Markdown Service', () => {
-  const processor = new MarkdownProcessor('/assets/markdown');
+  const processor = new MarkdownProcessor('/assets/markdown/');
   it('should find the right file in assets', () => {
     const actual = processor
       .processString(
         '_docs/form-controls/autocomplete/documentation.md'
       );
 
-    const expected = '_docs-form-controls-autocomplete-documentation.txt';
+    const expected = '/assets/markdown/_docs-form-controls-autocomplete-documentation.txt';
 
     expect(actual).toEqual(expected);
   });

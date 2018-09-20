d3afee083279d98ce4210f2e3bb55af92f1dae06
diegoaruiz <diego@reisystems.com>
2018-09-20 11:03:21 -0400

    initial update

---
+++ src/_docs/experimental/alert/component-example.ts
@@ -0,0 +1,14 @@
+
+/* tslint:disable */
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'doc-sam-alert-next',
+  template: ''
+})
+export class SamAlertNextComponentExampleComponent {
+  
+}
+

c31616596c8b4c5c06a600773f11479dd8f66d45
diegoaruiz <diego@reisystems.com>
2018-10-11 17:38:35 -0400

    example update

--- src/_docs/layout/filters-wrapper/component-example.ts
+++ src/_docs/layout/filters-wrapper/component-example.ts
@@ -1,22 +1,47 @@
 
 import {
-  Component, OnInit
+  Component, OnInit, forwardRef
 } from '@angular/core';
 import {
   FormBuilder
 } from '@angular/forms';
+import {
+  SamPageNextService,
+  DataStore,
+  layoutStore
+} from '@gsa-sam/sam-ui-elements';
 
 @Component({
   selector: 'doc-sam-filters-wrapper',
-  templateUrl: './component-example.html'
+  templateUrl: './component-example.html',
+  providers: [
+    {
+      provide: DataStore,
+      useValue: layoutStore
+    },
+    forwardRef(() => SamPageNextService)
+  ]
 })
 export class SamFiltersWrapperComponentExampleComponent implements OnInit {
   formGroup;
-  constructor(public fb: FormBuilder) {}
+  message = "";
+  submitted = false;
+  
+  constructor(public fb: FormBuilder, public service: SamPageNextService) {}
 
   ngOnInit() {
     this.formGroup = this.fb.group({
-      test: ''
+      testFilter: ''
+    });
+    
+    this.service.model.properties.filters.valueChanges.subscribe(data=>{
+      if(data && data.testFilters){
+        this.message = "form submitted";
+        this.submitted = true;
+      } else {
+        this.message = "form resetted";
+        this.submitted = false;
+      }
     });
   }
 }

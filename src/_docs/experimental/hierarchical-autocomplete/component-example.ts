c3578ce2980fd33e2d96004217eabeb899b93d10
Thomas Tighe <ttighe@reisystems.com>
2018-11-28 11:35:58 -0500

    adding service

---
+++ src/_docs/experimental/hierarchical-autocomplete/component-example.ts
@@ -0,0 +1,16 @@
+
+import { Component } from '@angular/core';
+import { HierarchicalDataService } from '../../services/hierarchical.service';
+
+
+
+@Component({
+  selector: 'doc-sam-autocomplete-hierarchical',
+  templateUrl: './component-example.html'
+})
+export class SamHierarchicalAutocompleteComponentExampleComponent {
+
+  constructor(public service: HierarchicalDataService) { 
+   
+  }
+}
--- src/_docs/services/hierarchical.service.ts
+++ src/_docs/services/hierarchical.service.ts
@@ -1,20 +1,39 @@
+/* tslint:disable */
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SamHiercarchicalServiceInterface } from '@gsa-sam/sam-ui-elements/src/ui-kit/experimental/hierarchical/hierarchical-interface';
-
+import { SampleHierarchicalData } from './hierarchical.data';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class HierarchicalDataService implements SamHiercarchicalServiceInterface {
 
 
     getDataByText(searchValue?: string): Observable<object[]> {
-        throw new Error("Method not implemented.");
+        let data = Observable.of(SampleHierarchicalData);
+        if (searchValue) {
+            data = data.map(items => items.filter(itm =>
+                (itm.name.indexOf(searchValue) !== -1 ||
+                    itm.subtext.indexOf(searchValue) !== -1
+                )))
+        }
+        return data;
     }
 
 
     getHiercarchicalById(id?: string): Observable<object[]> {
-        throw new Error("Method not implemented.");
+        let data = Observable.of(SampleHierarchicalData);
+        if (id) {
+
+
+            data = data.map(items => items.filter(itm => itm.parentId === id))
+        }
+        return data;
+    }
+
+    getTest() {
+        return 'Test';
     }
 
 }

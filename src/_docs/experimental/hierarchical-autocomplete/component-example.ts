8b165262cb6e8b60bef0d269da0f213eaf90398b
Thomas Tighe <ttighe@reisystems.com>
2018-12-05 09:57:44 -0500

    removing label

--- src/_docs/experimental/hierarchical-autocomplete/component-example.ts
+++ src/_docs/experimental/hierarchical-autocomplete/component-example.ts
@@ -24,22 +24,14 @@ export class SamHierarchicalAutocompleteComponentExampleComponent implements OnI
 
 
   ngOnInit() {
-    this.settings.keyField = "id";
-    // this.settings.erorMessage = "Error AutoComplete 1";
+    this.settings.keyField = "id";  
     this.settings.id = "autocomplete1";
-    // this.settings.labelShowFullHint
     this.settings.labelText = "Autocomplete 1;"
-    // this.settings.labelhint = "Some hint";
-    // this.settings.required = true;
     this.model.treeMode = TreeMode.SINGLE;
 
     this.settings2.keyField = "id";
-    // this.settings2.erorMessage = "Error AutoComplete 2";
-    this.settings2.id = "autocomplete2"
-    this.settings2.labelShowFullHint = false;
+    this.settings2.id = "autocomplete2"  
     this.settings2.labelText = "Autocomplete 2";
-    this.settings2.labelhint = "Some hint";
-    // this.settings2.required  = true;
     this.model2.treeMode = TreeMode.SINGLE;
   }
 }

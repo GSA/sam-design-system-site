a5b039d6ce12210c4594e5645bcf38c11607c192
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2018-12-05 11:38:23 -0500

    hierarchy grid changes

--- src/_docs/experimental/hierarchical-tree-grid/component-example.ts
+++ src/_docs/experimental/hierarchical-tree-grid/component-example.ts
@@ -2,73 +2,14 @@
 import {
   Component
 } from '@angular/core';
+import { HierarchicalDataService } from '../../services/hierarchical.service';
 
 @Component({
   selector: 'doc-sam-tree-grid',
   templateUrl: './component-example.html'
 })
 export class SamHierarchicalTreeGridComponentExampleComponent {
+  constructor(public service: HierarchicalDataService) {
 
-  example1_model = [
-    {
-      label: 'Share',
-      icon: 'fa-share-alt'
-    },
-    {
-      label: 'Download',
-      icon: 'fa-download'
-    },
-    {
-      label: 'Save',
-      icon: 'fa-cloud'
-    },
-    {
-      label: 'Toggle',
-      icon: 'fa-bars'
-    }
-  ];
-
-  example2_model = [
-    {
-      label: 'Share',
-      icon: 'fa-share-alt',
-      disabled: true
-    },
-    {
-      label: 'Download',
-      icon: 'fa-download',
-      disabled: true
-    },
-    {
-      label: 'Save',
-      icon: 'fa-cloud',
-      disabled: true
-    },
-    {
-      label: 'Toggle',
-      icon: 'fa-bars',
-      disabled: true
-    }
-  ];
-
-  example3_model = [
-    {
-      label: 'Share',
-      icon: 'fa-share-alt',
-      showMore: true
-    },
-    {
-      label: 'Download',
-      icon: 'fa-download'
-    },
-    {
-      label: 'Save',
-      icon: 'fa-cloud'
-    },
-    {
-      label: 'Toggle',
-      icon: 'fa-bars',
-      showMore: true
-    }
-  ];
+  }
 }

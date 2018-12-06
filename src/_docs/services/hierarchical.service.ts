e4ee05bbf7e237149ae906ecd3e2b2995a48d29d
Thomas Tighe <ttighe@reisystems.com>
2018-12-06 08:43:26 -0500

    add new model for paging

--- src/_docs/services/hierarchical.service.ts
+++ src/_docs/services/hierarchical.service.ts
@@ -1,7 +1,7 @@
 /* tslint:disable */
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { SamHiercarchicalServiceInterface } from '@gsa-sam/sam-ui-elements/src/ui-kit/experimental/hierarchical/hierarchical-interface';
+import { SamHiercarchicalServiceInterface, SearchByTextResult } from '@gsa-sam/sam-ui-elements/src/ui-kit/experimental/hierarchical/hierarchical-interface';
 
 import { SampleHierarchicalData } from './hierarchical.data';
 import 'rxjs/add/observable/of';
@@ -9,17 +9,41 @@ import 'rxjs/add/observable/of';
 @Injectable()
 export class HierarchicalDataService implements SamHiercarchicalServiceInterface {
 
-    getDataByText(searchValue?: string): Observable<object[]> {
+    getDataByText(searchValue?: string): Observable<SearchByTextResult> {
+        let itemIncrease = 25;
+        // items: object[];
+        // totalItems: number;
         let data = Observable.of(SampleHierarchicalData);
-
+        let itemsOb: Observable<Object[]>;
         if (searchValue) {
-            return data.map(items => items.filter(itm =>
+            itemsOb = data.map(items => items.filter(itm =>
                 (itm.name.indexOf(searchValue) !== -1 ||
                     itm.subtext.indexOf(searchValue) !== -1
                 )));
         } else {
-            return data;
+            itemsOb = data;
         }
+
+        let items: object[];
+        itemsOb.subscribe(
+            (result) => {
+                items = result;
+            }
+
+        );
+
+
+        let totalItemCount = items.length;
+
+        let returnItem = {
+
+            items: items,
+            totalItems: totalItemCount
+        };
+
+
+        return Observable.of(returnItem);
+
     }
 
     getHiercarchicalById(id?: string): Observable<object[]> {

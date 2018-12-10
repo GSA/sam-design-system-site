6a68ab8d08f1013e55a7cb7248241f03c05e3b95
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2018-12-10 14:33:11 -0500

    removed unused code

--- src/_docs/experimental/hierarchical-tree-grid/component-example.ts
+++ src/_docs/experimental/hierarchical-tree-grid/component-example.ts
@@ -3,12 +3,7 @@ import {
   Component, OnInit, ViewChild
 } from '@angular/core';
 import { HierarchicalDataService } from '../../services/hierarchical.service';
-import { DataSource } from '@angular/cdk';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { Observable } from 'rxjs-compat';
 
-import { MdPaginator } from '@angular/material';
-import { SamSortDirective } from '@gsa-sam/sam-ui-elements';
 import 'rxjs/add/observable/merge';
 import 'rxjs/add/operator/map';
 
@@ -17,8 +12,6 @@ import 'rxjs/add/operator/map';
   templateUrl: './component-example.html'
 })
 export class SamHierarchicalTreeGridComponentExampleComponent implements OnInit {
-  @ViewChild(MdPaginator) _paginator: MdPaginator;
-  @ViewChild(SamSortDirective) _sort: SamSortDirective;
   data: any[];
   result: any = {};
 
@@ -37,7 +30,7 @@ export class SamHierarchicalTreeGridComponentExampleComponent implements OnInit 
   }
 
   onSelectedItem(item) {
-    this.result = {};
+   // this.result = {};
     this.result = item;
   }
 

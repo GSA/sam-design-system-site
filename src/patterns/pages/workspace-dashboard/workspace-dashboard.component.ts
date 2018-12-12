38b62e0bcfe5f1ffbdf258097f81ca274cb7c93e
samanthabaker <samantha.lynneb@gmail.com>
2018-12-12 11:43:40 -0500

    created the workspace dashboard

--- src/patterns/pages/workspace-dashboard/workspace-dashboard.component.ts
+++ src/patterns/pages/workspace-dashboard/workspace-dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component} from '@angular/core';
 
 @Component({
-  templateUrl: 'workspace-dashboard.template.html'
+  templateUrl: 'workspace-dashboard.template.html',
+  styleUrls: ['workspace-dashboard.css']
 })
 export class SamWorkspaceDashboardComponent {
 

01d0a6ed263aa942982e87712bedf2570f645c97
Thomas Tighe <ttighe@reisystems.com>
2018-12-12 15:29:46 -0500

    Results now shown of selected items in autocomplete

--- src/_docs/experimental/hierarchical-autocomplete/component-example.ts
+++ src/_docs/experimental/hierarchical-autocomplete/component-example.ts
@@ -4,7 +4,7 @@ import { HierarchicalDataService } from '../../services/hierarchical.service';
 /* tslint:disable */
 import { HierarchicalTreeSelectedItemModel, TreeMode } from '@gsa-sam/sam-ui-elements/src/ui-kit/experimental/hierarchical/hierarchical-tree-selectedItem.model';
 import { SamHierarchicalAutocompleteSettings } from '@gsa-sam/sam-ui-elements/src/ui-kit/experimental/hierarchical/autocomplete/autocomplete.component';
-
+import { SelectedResultSettings } from '@gsa-sam/sam-ui-elements/src/ui-kit/experimental/hierarchical/selected-result/selected-result.component';
 
 
 @Component({
@@ -18,6 +18,16 @@ export class SamHierarchicalAutocompleteComponentExampleComponent implements OnI
 
   public model2 = new HierarchicalTreeSelectedItemModel();
   public settings2 = new SamHierarchicalAutocompleteSettings();
+
+  public model3 = new HierarchicalTreeSelectedItemModel();
+  public settings3 = new SamHierarchicalAutocompleteSettings();
+
+  public model4 = new HierarchicalTreeSelectedItemModel();
+  public settings4 = new SamHierarchicalAutocompleteSettings();
+
+  public resultSettings = new SelectedResultSettings();
+
+
 
   constructor(public service: HierarchicalDataService) {
 
@@ -36,5 +46,25 @@ export class SamHierarchicalAutocompleteComponentExampleComponent implements OnI
     this.settings2.id = 'autocomplete2';
     this.settings2.labelText = 'Autocomplete 2';
     this.model2.treeMode = TreeMode.SINGLE;
+    this.settings2.valueProperty = 'name';
+    this.settings2.subValueProperty = 'subtext';
+
+    this.settings3.keyField = 'id';
+    this.settings3.id = 'autocomplete3';
+    this.settings3.labelText = 'Autocomplete 3';
+    this.model3.treeMode = TreeMode.MULTIPLE;
+    this.settings3.valueProperty = 'name';
+    this.settings3.subValueProperty = 'subtext';
+
+    this.settings4.keyField = 'id';
+    this.settings4.id = 'autocomplete4';
+    this.settings4.labelText = 'Autocomplete 4';
+    this.model4.treeMode = TreeMode.MULTIPLE;
+    this.settings4.valueProperty = 'name';
+    this.settings4.subValueProperty = 'subtext';
+
+    this.resultSettings.keyField = 'id';
+    this.resultSettings.valueProperty = 'name';
+    this.resultSettings.subValueProperty = 'subtext';
   }
 }

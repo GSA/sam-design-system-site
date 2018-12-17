8bbfb0637d3861722de41076c7ef737b347c176e
Colin Alford <colinalford@gmail.com>
2018-12-17 14:55:43 -0500

    WIP

--- src/_docs/experimental/hierarchical-tree-grid/component-example.ts
+++ src/_docs/experimental/hierarchical-tree-grid/component-example.ts
@@ -5,61 +5,80 @@ import { HierarchicalDataService } from '../../services/hierarchical.service';
 import 'rxjs/add/observable/merge';
 import 'rxjs/add/operator/map';
 import { OptionsType } from '@gsa-sam/sam-ui-elements/src/ui-kit/types';
+import { Subscription, Observable, Subject, of, BehaviorSubject } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 @Component({
   selector: 'doc-sam-tree-grid',
   templateUrl: './component-example.html'
 })
-export class SamHierarchicalTreeGridComponentExampleComponent implements OnInit, AfterViewInit {
+export class SamHierarchicalTreeGridComponentExampleComponent implements OnInit {
   data: any[];
   result: any = {};
   selectedAgency: number;
-  selectConfig = {
-    options: []
-  };
+  options = [];
 
-  constructor(public service: HierarchicalDataService) {
-    this.service.getHiercarchicalById(null).subscribe(
-      (res) => {
-        this.data = res;
-      });
-  }
+  public tableData$: Observable<any>;
+  public selectedAgency$ = new BehaviorSubject<any>(null);
+
+  constructor(public service: HierarchicalDataService) {}
 
   configurations: any = {
     displayedColumns: ['id', 'name', 'subtext'],
   };
+
   public ngOnInit() {
+    this.tableData$ = this.selectedAgency$.pipe(
+      switchMap(agencyId => this.service.getHiercarchicalById(agencyId)),
+    );
+
+    this.tableData$.subscribe(
+      data => this.setOptionsData(data)
+    );
   }
 
-  public ngAfterViewInit() {
-    this.selectConfig.options = this.getOptionsData(this.data);
-  }
   onSelectedItem(item) {
     this.result = item;
   }
+
+  setOptionsData (data: any[]): void {
+    this.options = this.getOptionsData(data);
+  }
+
   getOptionsData(data: any[]): OptionsType[] {
     const temp = [];
-    data.forEach((ele) => {
-      if (ele.id) {
-        const item = {};
-        item['name'] = ele.name;
-        item['id'] = ele.id;
-        item['value'] = ele.id;
-        item['label'] = ele.subtext;
-        temp.push(item);
-      }
-    });
-    return temp;
-  }
-  onAgencySelect(agecySelectedItem) {
-    this.selectedAgency = agecySelectedItem;
-    this.getData('2');
-  }
-  getData(id: string | null): void {
-    this.data = [];
-    this.service.getHiercarchicalById(id).subscribe(
-      (res) => {
-        this.data = res;
-        this.selectConfig.options = this.getOptionsData(this.data);
+    if (!data || data.length === 0) {
+      return temp;
+    } else {
+      data.forEach((ele) => {
+        console.log(ele);
+        if (ele.id) {
+          const item = {};
+          item['name'] = ele.name;
+          item['id'] = ele.id;
+          item['value'] = ele.id;
+          item['label'] = ele.subtext;
+          temp.push(item);
+        }
       });
+      return temp;
+    }
   }
+
+  // onAgencySelect(agecySelectedItem) {
+  //   this.selectedAgency = agecySelectedItem;
+
+  //   this.service.getHiercarchicalById(agecySelectedItem).subscribe(
+  //     (res) => {
+  //       this.data = res;
+  //     });
+  // }
+
+  // getData(id: string | null): void {
+  //   this.data = [];
+  //   // this.tableData$.next(id);
+  //   this.service.getHiercarchicalById(id).subscribe(
+  //     (res) => {
+  //       this.data = res;
+  //     });
+  // }
 }

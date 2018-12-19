17155ce93155fca5296448d18569f2f39c0afdd9
Thomas Tighe <ttighe@reisystems.com>
2018-12-19 08:32:07 -0500

    update dynamic and cleanup test

--- src/_docs/experimental/hierarchical-autocomplete/component-example.ts
+++ src/_docs/experimental/hierarchical-autocomplete/component-example.ts
@@ -41,7 +41,7 @@ export class SamHierarchicalAutocompleteComponentExampleComponent implements OnI
     this.model.treeMode = TreeMode.SINGLE;
     this.settings.valueProperty = 'name';
     this.settings.subValueProperty = 'subtext';
-    this.settings.placeHolderText = "Fill";
+    this.settings.placeHolderText = "Autocomplete 1";
 
     this.settings2.keyField = 'id';
     this.settings2.id = 'autocomplete2';
@@ -49,6 +49,7 @@ export class SamHierarchicalAutocompleteComponentExampleComponent implements OnI
     this.model2.treeMode = TreeMode.SINGLE;
     this.settings2.valueProperty = 'name';
     this.settings2.subValueProperty = 'subtext';
+    this.settings2.placeHolderText = "Autocomplete 2";
 
     this.settings3.keyField = 'id';
     this.settings3.id = 'autocomplete3';
@@ -56,6 +57,7 @@ export class SamHierarchicalAutocompleteComponentExampleComponent implements OnI
     this.model3.treeMode = TreeMode.MULTIPLE;
     this.settings3.valueProperty = 'name';
     this.settings3.subValueProperty = 'subtext';
+    this.settings3.placeHolderText = "Autocomplete 3";
 
     this.settings4.keyField = 'id';
     this.settings4.id = 'autocomplete4';
@@ -63,6 +65,7 @@ export class SamHierarchicalAutocompleteComponentExampleComponent implements OnI
     this.model4.treeMode = TreeMode.MULTIPLE;
     this.settings4.valueProperty = 'name';
     this.settings4.subValueProperty = 'subtext';
+    this.settings4.placeHolderText = "Autocomplete 4";
 
     this.resultSettings.keyField = 'id';
     this.resultSettings.valueProperty = 'name';

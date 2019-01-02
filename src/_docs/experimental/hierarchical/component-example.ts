9d4c1b238cb737a5a6e23023210c6cb9cdf90129
Thomas Tighe <ttighe@reisystems.com>
2019-01-02 15:38:22 -0500

    adding sample controls

--- src/_docs/experimental/hierarchical/component-example.ts
+++ src/_docs/experimental/hierarchical/component-example.ts
@@ -3,8 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { HierarchicalDataService } from '../../services/hierarchical.service';
 /* tslint:disable */
 import { HierarchicalTreeSelectedItemModel, TreeMode } from '@gsa-sam/sam-ui-elements/src/ui-kit/experimental/hierarchical/hierarchical-tree-selectedItem.model';
-import { SamHierarchicalAutocompleteSettings } from '@gsa-sam/sam-ui-elements/src/ui-kit/experimental/hierarchical/autocomplete/autocomplete.component';
-import { SelectedResultSettings } from '@gsa-sam/sam-ui-elements/src/ui-kit/experimental/hierarchical/selected-result/selected-result.component';
+import { SamHierarchicalSettings } from '@gsa-sam/sam-ui-elements/src/ui-kit/experimental/hierarchical/hierarchical/hierarchical.component';
+
 
 
 @Component({
@@ -13,19 +13,16 @@ import { SelectedResultSettings } from '@gsa-sam/sam-ui-elements/src/ui-kit/expe
 })
 export class SamHierarchicalComponentExampleComponent implements OnInit {
 
-  // public model = new HierarchicalTreeSelectedItemModel();
-  // public settings = new SamHierarchicalAutocompleteSettings();
 
-  // public model2 = new HierarchicalTreeSelectedItemModel();
-  // public settings2 = new SamHierarchicalAutocompleteSettings();
+  public settings = new SamHierarchicalSettings();
+  public settings2 = new SamHierarchicalSettings();
+  public settings3 = new SamHierarchicalSettings();
+  public settings4 = new SamHierarchicalSettings();
 
-  // public model3 = new HierarchicalTreeSelectedItemModel();
-  // public settings3 = new SamHierarchicalAutocompleteSettings();
-
-  // public model4 = new HierarchicalTreeSelectedItemModel();
-  // public settings4 = new SamHierarchicalAutocompleteSettings();
-
-  // public resultSettings = new SelectedResultSettings();
+  public model = new HierarchicalTreeSelectedItemModel();
+  public model2 = new HierarchicalTreeSelectedItemModel();
+  public model3 = new HierarchicalTreeSelectedItemModel();
+  public model4 = new HierarchicalTreeSelectedItemModel();
 
 
 
@@ -35,40 +32,38 @@ export class SamHierarchicalComponentExampleComponent implements OnInit {
 
 
   ngOnInit() {
-    // this.settings.keyField = 'id';
-    // this.settings.id = 'autocomplete1';
-    // this.settings.labelText = 'Autocomplete 1';
-    // this.model.treeMode = TreeMode.SINGLE;
-    // this.settings.valueProperty = 'name';
-    // this.settings.subValueProperty = 'subtext';
-    // this.settings.placeHolderText = "Autocomplete 1";
+    this.settings.keyField = 'id';
+    this.settings.id = 'autocomplete1';
+    this.settings.labelText = 'Autocomplete 1';  
+    this.settings.valueProperty = 'name';
+    this.settings.subValueProperty = 'subtext';
+    this.settings.placeHolderText = "Enter text";
 
-    // this.settings2.keyField = 'id';
-    // this.settings2.id = 'autocomplete2';
-    // this.settings2.labelText = 'Autocomplete 2';
-    // this.model2.treeMode = TreeMode.SINGLE;
-    // this.settings2.valueProperty = 'name';
-    // this.settings2.subValueProperty = 'subtext';
-    // this.settings2.placeHolderText = "Autocomplete 2";
+    this.settings2.keyField = 'id';
+    this.settings2.id = 'autocomplete2';
+    this.settings2.labelText = 'Autocomplete 2';  
+    this.settings2.valueProperty = 'name';
+    this.settings2.subValueProperty = 'subtext';
+    this.settings2.placeHolderText = "Enter text";
+    
+    this.settings3.keyField = 'id';
+    this.settings3.id = 'autocomplete3';
+    this.settings3.labelText = 'Autocomplete 3';  
+    this.settings3.valueProperty = 'name';
+    this.settings3.subValueProperty = 'subtext';
+    this.settings3.placeHolderText = "Enter text";
 
-    // this.settings3.keyField = 'id';
-    // this.settings3.id = 'autocomplete3';
-    // this.settings3.labelText = 'Autocomplete 3';
-    // this.model3.treeMode = TreeMode.MULTIPLE;
-    // this.settings3.valueProperty = 'name';
-    // this.settings3.subValueProperty = 'subtext';
-    // this.settings3.placeHolderText = "Autocomplete 3";
+    this.settings4.keyField = 'id';
+    this.settings4.id = 'autocomplete4';
+    this.settings4.labelText = 'Autocomplete 4';  
+    this.settings4.valueProperty = 'name';
+    this.settings4.subValueProperty = 'subtext';
+    this.settings4.placeHolderText = "Enter text";
 
-    // this.settings4.keyField = 'id';
-    // this.settings4.id = 'autocomplete4';
-    // this.settings4.labelText = 'Autocomplete 4';
-    // this.model4.treeMode = TreeMode.MULTIPLE;
-    // this.settings4.valueProperty = 'name';
-    // this.settings4.subValueProperty = 'subtext';
-    // this.settings4.placeHolderText = "Autocomplete 4";
 
-    // this.resultSettings.keyField = 'id';
-    // this.resultSettings.valueProperty = 'name';
-    // this.resultSettings.subValueProperty = 'subtext';
+    this.model.treeMode = TreeMode.SINGLE;
+    this.model2.treeMode = TreeMode.SINGLE;
+    this.model3.treeMode = TreeMode.MULTIPLE;
+    this.model4.treeMode = TreeMode.MULTIPLE;
   }
 }

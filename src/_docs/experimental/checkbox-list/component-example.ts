d66846caae1ad7598f013d8809377eedfcb0649d
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2019-01-31 15:45:40 -0500

    selceted results added

--- src/_docs/experimental/checkbox-list/component-example.ts
+++ src/_docs/experimental/checkbox-list/component-example.ts
@@ -3,12 +3,30 @@
 import {
   Component
 } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'doc-sam-checkbox-list',
   templateUrl: './component-example.html'
 })
 export class SamCheckboxListComponentExampleComponent {
-  testheader:string ='test';
+  accordionText:string ='test';
+  public selectItems: any[];
+  public options = [
+    { name: 'id1', value: 'test1', label: 'test-id1', required: false, checked: false },
+    { name: 'id2', value: 'test2', label: 'test-id2', required: true, checked: true },
+    { name: 'id3', value: 'test3', label: 'test-id3', required: false, checked: false },
+    { name: 'id4', value: 'test4', label: 'test-id4', required: false, checked: false },
+    { name: 'id5', value: 'test5', label: 'test-id5', required: false, checked: true },
+    { name: 'id6', value: 'test6', label: 'test-id6', required: false, checked: false },
+    { name: 'id7', value: 'test7', label: 'test-id7', required: false, checked: false },
+    { name: 'id8', value: 'test8', label: 'test-id8', required: false, checked: false },
+  ];
+  public selectResults$ = new BehaviorSubject<any[]>([]);
+  public ngOnInit() {
+    
+    this.selectResults$.subscribe(res => this.selectItems = res);
+  }
+
 }
 

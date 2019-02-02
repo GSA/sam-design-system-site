d6c78bda191ba8cd80d1f0d34552f867ac6f1e50
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2019-02-02 08:49:27 -0500

    single mode changes

--- src/_docs/experimental/checkbox-list/component-example.ts
+++ src/_docs/experimental/checkbox-list/component-example.ts
@@ -12,6 +12,7 @@ import { Observable, BehaviorSubject } from 'rxjs';
 export class SamCheckboxListComponentExampleComponent {
   accordionText:string ='test';
   public selectItems: any[];
+  public selectItems1: any[];
   public options = [
     { name: 'id1', value: 'test1', label: 'test-id1', required: false, checked: false },
     { name: 'id2', value: 'test2', label: 'test-id2', required: true, checked: true },
@@ -22,10 +23,24 @@ export class SamCheckboxListComponentExampleComponent {
     { name: 'id7', value: 'test7', label: 'test-id7', required: false, checked: false },
     { name: 'id8', value: 'test8', label: 'test-id8', required: false, checked: false },
   ];
+
+  public options1 = [
+    { name: 'id1', value: 'test1', label: 'test-id1', required: false, checked: false },
+    { name: 'id2', value: 'test2', label: 'test-id2', required: true, checked: true },
+    { name: 'id3', value: 'test3', label: 'test-id3', required: false, checked: false },
+    { name: 'id4', value: 'test4', label: 'test-id4', required: false, checked: false },
+    { name: 'id5', value: 'test5', label: 'test-id5', required: false, checked: true },
+    { name: 'id6', value: 'test6', label: 'test-id6', required: false, checked: false },
+    { name: 'id7', value: 'test7', label: 'test-id7', required: false, checked: false },
+    { name: 'id8', value: 'test8', label: 'test-id8', required: false, checked: false },
+  ];
+
   public selectResults$ = new BehaviorSubject<any[]>([]);
+  public selectResults1$ = new BehaviorSubject<any[]>([]);
   public ngOnInit() {
     
     this.selectResults$.subscribe(res => this.selectItems = res);
+    this.selectResults1$.subscribe(res => this.selectItems1 = res);
   }
 
 }

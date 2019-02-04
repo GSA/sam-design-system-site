3da251c8bfbda016f518aa8ec089ef0d194315d6
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2019-02-04 09:51:32 -0500

    radio button group changes

--- src/_docs/experimental/checkbox-list/component-example.ts
+++ src/_docs/experimental/checkbox-list/component-example.ts
@@ -29,7 +29,7 @@ export class SamCheckboxListComponentExampleComponent {
     { name: 'id2', value: 'test2', label: 'test-id2', required: true, checked: true },
     { name: 'id3', value: 'test3', label: 'test-id3', required: false, checked: false },
     { name: 'id4', value: 'test4', label: 'test-id4', required: false, checked: false },
-    { name: 'id5', value: 'test5', label: 'test-id5', required: false, checked: true },
+    { name: 'id5', value: 'test5', label: 'test-id5', required: false, checked: false },
     { name: 'id6', value: 'test6', label: 'test-id6', required: false, checked: false },
     { name: 'id7', value: 'test7', label: 'test-id7', required: false, checked: false },
     { name: 'id8', value: 'test8', label: 'test-id8', required: false, checked: false },

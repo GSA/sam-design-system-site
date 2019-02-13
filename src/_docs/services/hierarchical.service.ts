14714ee22246a339195549355825a77f55a37961
Thomas Tighe <ttighe@reisystems.com>
2019-02-13 13:58:55 -0500

    adding continuous scroll for service advanced

--- src/_docs/services/hierarchical.service.ts
+++ src/_docs/services/hierarchical.service.ts
@@ -1,7 +1,7 @@
 /* tslint:disable */
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { SamHiercarchicalServiceInterface, SearchByTextResult } from '@gsa-sam/sam-ui-elements/src/ui-kit/experimental/hierarchical/hierarchical-interface';
+import { SamHiercarchicalServiceInterface, SamHiercarchicalServiceResult } from '@gsa-sam/sam-ui-elements/src/ui-kit/experimental/hierarchical/hierarchical-interface';
 import { Sort } from '@gsa-sam/sam-ui-elements/src/ui-kit/components/data-table/sort.directive'
 
 import { SampleHierarchicalData } from './hierarchical.data';
@@ -22,7 +22,7 @@ export class HierarchicalDataService implements SamHiercarchicalServiceInterface
         this.loadedData = data;
     }
 
-    getDataByText(currentItems: number, searchValue?: string): Observable<SearchByTextResult> {
+    getDataByText(currentItems: number, searchValue?: string): Observable<SamHiercarchicalServiceResult> {
         let itemIncrease = 25;
         let data = Observable.of(this.loadedData);
         let itemsOb: Observable<Object[]>;
@@ -55,18 +55,44 @@ export class HierarchicalDataService implements SamHiercarchicalServiceInterface
         return Observable.of(returnItem);
     }
 
-    getHiercarchicalById(id: string, searchValue: string, sort: Sort): Observable<object[]> {
+    getHiercarchicalById(id: string, searchValue: string, sort: Sort, currentItemCount: number): Observable<SamHiercarchicalServiceResult> {
+        let itemIncrease = 15;
         let temp = this.getSortedData(this.loadedData, sort);
         let data = Observable.of(temp);
+        let itemsOb: Observable<Object[]>;
         if (searchValue) {
-            return data.map(items => items.filter(itm => itm.parentId === id && (itm.name.indexOf(searchValue) !== -1 || itm.subtext.indexOf(searchValue) !== -1)));
+            itemsOb = data.map(items => items.filter(itm =>
+                itm.parentId === id &&
+                (itm.name.indexOf(searchValue) !== -1 ||
+                    itm.subtext.indexOf(searchValue) !== -1
+                )));
         } else {
-            return data.map(items => items.filter(itm => itm.parentId === id));
+            itemsOb = data.map(items => items.filter(itm => itm.parentId === id));
         }
+        let items: object[];
+        itemsOb.subscribe(
+            (result) => {
+                items = result;
+            }
+        );
+        let totalItemCount = items.length;
+
+        let maxSectionPosition = currentItemCount + itemIncrease;
+        if (maxSectionPosition > totalItemCount) {
+            maxSectionPosition = totalItemCount;
+        }
+        let subItemsitems = items.slice(currentItemCount, maxSectionPosition);
+
+        let returnItem = {
+            items: subItemsitems,
+            totalItems: totalItemCount
+        };
+        console.log(returnItem);
+        return Observable.of(returnItem);
     }
 
 
-    getSortedData(data: any[], sort: Sort): any[] {
+    private getSortedData(data: any[], sort: Sort): any[] {
         if (!sort || (!sort.active || sort.direction === '')) { return data; }
         return data.sort((a, b) => {
             let propertyA = this.sortingDataAccessor(a, sort.active);
@@ -77,7 +103,7 @@ export class HierarchicalDataService implements SamHiercarchicalServiceInterface
         });
     }
 
-    sortingDataAccessor: ((data: any, sortHeaderId: string) => string | number) =
+    private sortingDataAccessor: ((data: any, sortHeaderId: string) => string | number) =
         (data: any, sortHeaderId: string): string | number => {
             const value = (data as { [key: string]: any })[sortHeaderId];
             return value;

36aba1f350e1728ac89c4bccc42ffdf78050ba02
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2019-02-15 11:12:22 -0500

    selected date emit changes

--- src/_docs/experimental/date-range-v2/component-example.ts
+++ src/_docs/experimental/date-range-v2/component-example.ts
@@ -10,7 +10,29 @@ import { Observable, BehaviorSubject } from 'rxjs';
   templateUrl: './component-example.html'
 })
 export class SamDateRangeV2ExampleComponent {
+  startDateConfig: any = {
+    name: 'Start Date',
+    placeholder: 'Start Date',
+    label:'Start Date Picker',
+    hint:'',
+    date :'12/21/2001'
+  };
 
-  
+  endDateConfig: any = {
+    name: 'End Date',
+    placeholder: 'End Date',
+    label:'End Date Picker',
+    hint:'',
+    date :'12/21/2021'
+  };
+
+  public selectDates: object;
+
+  public selectedDates$ = new BehaviorSubject<any>([]);
+  public ngOnInit() {
+    this.selectedDates$.subscribe(res =>
+      this.selectDates = res
+    );
+  }
 }
 

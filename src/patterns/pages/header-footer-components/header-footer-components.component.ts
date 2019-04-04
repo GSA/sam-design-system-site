a36a46612e8d0d3f1a5fb9d3c63400faea1f5e99
Thomas Tighe <ttighe@reisystems.com>
2019-04-04 13:26:35 -0400

    adding routing for all things

--- src/patterns/pages/header-footer-components/header-footer-components.component.ts
+++ src/patterns/pages/header-footer-components/header-footer-components.component.ts
@@ -5,29 +5,29 @@ import { Component } from '@angular/core';
 })
 export class HeaderFooterAngualarComponent {
   model = {
-    secondaryLinks: [{ imageSourcePath: "assets/icon/header-footer/Messages.png", imageAltText: "Messages Icon", text: "Messages" },
-    { imageSourcePath: "assets/icon/header-footer/Requests.png", imageAltText: "Requests Icon", text: "Requests" },
-    { imageSourcePath: "assets/icon/header-footer/Workspace.png", imageAltText: "Workspace Icon", text: "Workspace" },
-    { imageSourcePath: "assets/icon/header-footer/SignOut.png", imageAltText: "Sign Out Icon", text: "Sign Out" }],
+    secondaryLinks: [{ imageSourcePath: "assets/icon/header-footer/Messages.png", imageAltText: "Messages Icon", text: "Messages", route: "/" },
+    { imageSourcePath: "assets/icon/header-footer/Requests.png", imageAltText: "Requests Icon", text: "Requests", route: "/" },
+    { imageSourcePath: "assets/icon/header-footer/Workspace.png", imageAltText: "Workspace Icon", text: "Workspace", route: "/" },
+    { imageSourcePath: "assets/icon/header-footer/SignOut.png", imageAltText: "Sign Out Icon", text: "Sign Out", route: "/" }],
     navigationLinks: [
       { text: "Search", route: "/" },
       { text: "Databank", route: "/" },
       { text: "Data Services", route: "/" },
       { text: "Help", route: "/" },
       {
-        text: "Link 3", route: "", children: [
-          { text: "Link 3 sub 1", route: "" },
-          { text: "Link 3 sub 2", route: "" },
-          { text: "Link 3 sub 3", route: "" },
-          { text: "Link 3 sub 4", route: "" }
+        text: "Link 3", route: "/", children: [
+          { text: "Link 3 sub 1", route: "/" },
+          { text: "Link 3 sub 2", route: "/" },
+          { text: "Link 3 sub 3", route: "/" },
+          { text: "Link 3 sub 4", route: "/" }
         ]
       },
       {
-        text: "Link 4", route: "", children: [
-          { text: "Link 4 sub 1", route: "" },
-          { text: "Link 4 sub 2", route: "" },
-          { text: "Link 4 sub 3", route: "" },
-          { text: "Link 4 sub 4", route: "" }
+        text: "Link 4", route: "/", children: [
+          { text: "Link 4 sub 1", route: "/" },
+          { text: "Link 4 sub 2", route: "/" },
+          { text: "Link 4 sub 3", route: "/" },
+          { text: "Link 4 sub 4", route: "/" }
         ]
       }
     ],

a5b760d06c50d651fea788855b162b90143b5592
Thomas Tighe <ttighe@reisystems.com>
2019-04-09 13:35:51 -0400

    adding more functionality

--- src/patterns/pages/header-footer-components/header-footer-components.component.ts
+++ src/patterns/pages/header-footer-components/header-footer-components.component.ts
@@ -1,33 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+
 
 @Component({
   templateUrl: 'header-footer-components.template.html'
 })
-export class HeaderFooterAngualarComponent {
+export class HeaderFooterAngualarComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    this.header.select(this.modelHeader.navigationLinks[0].id);
+  }
+
+
+  @ViewChild('header') header;
   modelHeader = {
-    secondaryLinks: [{ imageSourcePath: "assets/icon/header-footer/Messages.png", imageAltText: "Messages Icon", text: "Messages", route: "/" },
-    { imageSourcePath: "assets/icon/header-footer/Requests.png", imageAltText: "Requests Icon", text: "Requests", route: "/" },
-    { imageSourcePath: "assets/icon/header-footer/Workspace.png", imageAltText: "Workspace Icon", text: "Workspace", route: "/" },
-    { imageSourcePath: "assets/icon/header-footer/SignOut.png", imageAltText: "Sign Out Icon", text: "Sign Out", route: "/" }],
+    secondaryLinks: [{ imageSourcePath: "assets/icon/header-footer/Messages.png", imageAltText: "Messages Icon", text: "Messages", route: "/", id: "messages" },
+    { imageSourcePath: "assets/icon/header-footer/Requests.png", imageAltText: "Requests Icon", text: "Requests", route: "/", id: "request" },
+    { imageSourcePath: "assets/icon/header-footer/Workspace.png", imageAltText: "Workspace Icon", text: "Workspace", route: "/", id: "workspace" },
+    { imageSourcePath: "assets/icon/header-footer/SignOut.png", imageAltText: "Sign Out Icon", text: "Sign Out", route: "/", id: "signOut" }],
     navigationLinks: [
-      { text: "Search", route: "/" },
-      { text: "Databank", route: "/" },
-      { text: "Data Services", route: "/" },
-      { text: "Help", route: "/" },
+      { text: "Search", route: "/patterns/page/angualr-header-footer-component", id: "search" },
+      { text: "Databank", route: "/", id: "databank" },
+      { text: "Data Services", route: "/", id: "dataService" },
+      { text: "Help", route: "/", id: "help" },
       {
-        text: "Link 3", children: [
-          { text: "Link 3 sub 1", route: "/" },
-          { text: "Link 3 sub 2", route: "/" },
-          { text: "Link 3 sub 3", route: "/" },
-          { text: "Link 3 sub 4", route: "/" }
+        text: "Link 3", id: "link3", children: [
+          { text: "Link 3 sub 1", route: "/", id: "link3sub1" },
+          { text: "Link 3 sub 2", route: "/", id: "link3sub2" },
+          { text: "Link 3 sub 3", route: "/", id: "link3sub3" },
+          { text: "Link 3 sub 4", route: "/", id: "link3sub4" }
         ]
       },
       {
-        text: "Link 4", children: [
-          { text: "Link 4 sub 1", route: "/" },
-          { text: "Link 4 sub 2", route: "/" },
-          { text: "Link 4 sub 3", route: "/" },
-          { text: "Link 4 sub 4", route: "/" }
+        text: "Link 4", id: "link4", children: [
+          { text: "Link 4 sub 1", route: "/", id: "link4sub1" },
+          { text: "Link 4 sub 2", route: "/", id: "link4sub2" },
+          { text: "Link 4 sub 3", route: "/", id: "link4sub3" },
+          { text: "Link 4 sub 4", route: "/", id: "link4sub4" }
         ]
       }
     ],
@@ -35,7 +42,8 @@ export class HeaderFooterAngualarComponent {
       text: "Home",
       route: "/",
       imageSourcePath: "/assets/img/sam-r9-logo-color.png",
-      imageAltText: "Beta Sam Logo"
+      imageAltText: "Beta Sam Logo",
+      id: "home"
     }
   };
 

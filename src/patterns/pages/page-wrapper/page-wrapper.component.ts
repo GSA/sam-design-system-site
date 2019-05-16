c6a3b707cefc509c6a3e5fd16f7d727147201e43
Carlos Valle <carlos.m.valle@gmail.com>
2019-05-15 23:23:01 -0400

    Remove unused patterns

--- src/patterns/pages/page-wrapper/page-wrapper.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component} from '@angular/core';
-
-@Component({
-  templateUrl: 'page-wrapper.template.html'
-})
-export class PageWrapperComponent {}
--- src/patterns/pages/search-results/search-results.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component} from '@angular/core';
-
-@Component({
-  templateUrl: 'search-results.template.html'
-})
-export class SearchResultsComponent {
-
-}

6d40356dc390a2742a4b61dc97fe761c1c275358
Carlos Valle <carlos.m.valle@gmail.com>
2019-07-16 10:33:34 -0400

    Remove USWDS 2.0 dependencies

--- src/patterns/pages/workspace-dashboard/workspace-dashboard.component.ts
+++ src/patterns/pages/workspace-dashboard/workspace-dashboard.component.ts
@@ -1,8 +1,6 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   templateUrl: 'workspace-dashboard.template.html'
 })
-export class SamWorkspaceDashboardComponent {
-
-}
+export class SamWorkspaceDashboardComponent {}

2f93e0a4b17c161f13b2d6a207191533ad9a11b2
samanthabaker <samantha.lynneb@gmail.com>
2019-07-22 12:36:25 -0400

    side nav

--- src/patterns/pages/entity-results/entity-results.component.ts
+++ src/patterns/pages/entity-results/entity-results.component.ts
@@ -1,8 +1,81 @@
 import { Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 
+import {
+  SidenavService
+} from '@gsa-sam/sam-ui-elements';
+
+
 @Component({
   selector: 'sam-entity-results',
   templateUrl: 'entity-results.template.html'
 })
 
 export class SamEntityResultsComponent { }
+
+export class SamSidenavComponentExampleComponent {
+  config = {
+    label: 'stuff',
+    children: [
+      {
+        label: 'Item 1',
+        route: '/foo',
+        children: [
+          {
+            label: 'Child 1',
+            route: '/lorem',
+            children: [
+              {
+                label: 'Grandchild 1',
+                route: '/sit'
+              },
+              {
+                label: 'Grandchild 2',
+                route: '/amet'
+              },
+              {
+                label: 'Grandchild 3',
+                route: '/consectetur'
+              }
+            ]
+          },
+          {
+            label: 'Child 2',
+            route: '/ipsum',
+            children: [
+              {
+                label: 'Grandchild 1',
+                route: '/adipisicings'
+              }
+            ]
+          },
+          {
+            label: 'Child 3',
+            route: '/dolor'
+          }
+        ]
+      },
+      {
+        label: 'Item 2',
+        route: '/bar',
+        children: [
+          {
+            label: 'Child 1',
+            route: '/elit'
+          },
+          {
+            label: 'Child 2',
+            route: '/cumque'
+          },
+          {
+            label: 'Child 3',
+            route: '/dignissimos'
+          }
+        ]
+      },
+      {
+        label: 'Item 3',
+        route: '/baz'
+      }
+    ]
+  };
+}

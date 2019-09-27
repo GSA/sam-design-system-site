7e33987f968bca1d15656a12c0633dbbe5b341d8
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2019-09-27 11:59:33 -0400

    updated example

--- src/_docs/form-controls/textarea/component-example.ts
+++ src/_docs/form-controls/textarea/component-example.ts
@@ -1,18 +1,31 @@
 import {
-  Component
+  Component, OnInit
 } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'doc-textarea',
   templateUrl: './component-example.html'
 })
-export class SamTextareaComponentExampleComponent {
+export class SamTextareaComponentExampleComponent implements OnInit{
   textareaModel= 'Some Text';
-  textareaModel1= '';
+  orgType = 'department';
+  errors = {
+    description: 'required'
+  };
   textareaConfig = {
     label: 'Enter zipcode',
     hint: 'Zipcode can be short or long version',
     name: 'aria-friendly-name',
     disabled: false,
   };
+  basicInfoForm: FormGroup;
+  ngOnInit() {
+    this.basicInfoForm = new FormGroup({
+       orgDescription: new FormControl('', []),
+    });
+  }
+  isDepartment() {
+    return this.orgType === 'department';
+  }
 }

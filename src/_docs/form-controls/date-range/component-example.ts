b9c158b38e2c5586e870b6a73e0e8d580fda8ef5
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2021-07-07 13:51:16 -0400

    single qote lint

--- src/_docs/form-controls/date-range/component-example.ts
+++ src/_docs/form-controls/date-range/component-example.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "doc-sam-date-range",
-  templateUrl: "./component-example.html",
+  selector: 'doc-sam-date-range',
+  templateUrl: './component-example.html',
 })
 export class SamDateRangeComponentExampleComponent {
   public dateRangeModel = {
